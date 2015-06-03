dd4357463acc5f309ab7cc1424bc2a61996e99f7
Martin Poirier Theoret <mpoiriert@gmail.com>
2015-06-03 12:39:39 -0400

    more validation extractor configured

--- DependencyInjection/Compiler/ExtractorCompilerPass.php
+++ DependencyInjection/Compiler/ExtractorCompilerPass.php
@@ -4,6 +4,7 @@ namespace Draw\SwaggerBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Reference;
 
 class ExtractorCompilerPass implements CompilerPassInterface
@@ -18,8 +19,26 @@ class ExtractorCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
+        $swagger = $container->getDefinition("draw.swagger");
+
         foreach (array_keys($container->findTaggedServiceIds("swagger.extractor")) as $id) {
-            $container->getDefinition("draw.swagger")->addMethodCall("registerExtractor", array(new Reference($id)));
+            if ($container->getDefinition($id)->isAbstract()) {
+                continue;
+            }
+
+            $swagger->addMethodCall("registerExtractor", array(new Reference($id)));
+        }
+
+        foreach ($container->getDefinitions() as $id => $definition) {
+            if (!$definition instanceof DefinitionDecorator) {
+                continue;
+            }
+
+            if ($definition->getParent() != "draw.swagger.extractor.constraint_extractor") {
+                continue;
+            }
+
+            $swagger->addMethodCall("registerExtractor", array(new Reference($id)));
         }
     }
 }
