2c475a317db157b776f3c5689305d1c87ce9639a
Martin Poirier Theoret <mpoiriert@gmail.com>
2015-09-23 17:51:50 -0400

    redirect on swagger if route is not json

--- Controller/SwaggerController.php
+++ Controller/SwaggerController.php
@@ -4,11 +4,20 @@ namespace Draw\SwaggerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class SwaggerController extends Controller
 {
-    public function apiDocAction()
+    public function apiDocAction(Request $request)
     {
+        if($request->getRequestFormat() != 'json') {
+            $currentRoute = $request->attributes->get('_route');
+            $currentUrl = $this->get('router')
+                ->generate($currentRoute, array('_format' => 'json'), true);
+            return new RedirectResponse('http://petstore.swagger.io/?url=' . $currentUrl);
+        }
+        
         $swagger = $this->get("draw.swagger");
         $schema = $swagger->extract(json_encode($this->getParameter("draw_swagger.schema")));
 
