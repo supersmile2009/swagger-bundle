0a80fd9d6006d4104266f6c417bfb232fe032a82
supersmile2009 <supersmile2009@gmail.com>
2017-07-10 18:33:49 +0300

    PNP-12332 Extractor to handle abstract classes and their children (#2)

---
+++ Extractor/AbstractClassExtractor.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Draw\SwaggerBundle\Extractor;
+
+use Doctrine\ORM\EntityManager;
+use Draw\Swagger\Extraction\ExtractionContextInterface;
+use Draw\Swagger\Extraction\ExtractionImpossibleException;
+use Draw\Swagger\Extraction\ExtractorInterface;
+use Draw\Swagger\Schema\Schema;
+
+class AbstractClassExtractor implements ExtractorInterface
+{
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param string $className
+     * @param Schema $target
+     * @param ExtractionContextInterface $subContext
+     * @throws ExtractionImpossibleException
+     */
+    public function extract($className, $target, ExtractionContextInterface $subContext) {
+
+        if (!$this->canExtract($className, $target, $subContext)) {
+            throw new ExtractionImpossibleException();
+        }
+        $reflectionClass = new \ReflectionClass($className);
+
+        $metadata = $this->em->getClassMetadata($reflectionClass->getName());
+        foreach ($metadata->subClasses as $className) {
+            $subContext->getSwagger()->extract(
+                $className,
+                clone $target,
+                clone $subContext
+            );
+        }
+    }
+    public function canExtract($className, $target, ExtractionContextInterface $subContext)
+    {
+        if (is_string($className) && class_exists($className)) {
+            $reflectionClass = new \ReflectionClass($className);
+            return $reflectionClass->isAbstract();
+        }
+        return false;
+    }
+
+
+}
